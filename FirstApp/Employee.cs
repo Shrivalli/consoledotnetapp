def1c04060e31e7329678c5d865b789c2e8548b6
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2021-04-05 08:31:49 +0530

    First commit

---
+++ FirstApp/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstApp
+{
+    class Employee
+    {
+        public string Empname { get; set; }
+        public float Salary { get; set; }
+
+        public Employee()
+        {
+
+        }
+        public Employee(string ename, float sal)
+        {
+            Empname = ename;
+            Salary = sal;
+        }
+    }
+}
---
+++ FirstApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Employee e1 = new Employee();
+            e1.Empname = "Radha";
+            e1.Salary = 50000;
+            Employee e2 = new Employee("Vandhana", 230000);
+            Console.WriteLine(e1.Empname + "  " + e1.Salary);
+            Console.WriteLine(e2.Empname + " " + e2.Salary);
+            Console.ReadLine();
+        }
+    }
+}
